726a5ddc25aad96c368b14eefbc0a1fb94888adc
Abhishek <abhishekbulbule384@gmail.com>
2022-02-08 17:08:57 +0530

    Added entire small django project

---
+++ djangochat/chat/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from django.urls.resolvers import URLPattern
+from . import views
+
+
+urlpatterns =[
+    path('', views.home, name='home'),
+    path('<str:room>/', views.room, name='room'),
+    path('checkview', views.checkview, name='checkview'),
+    path('send', views.send, name='send'),
+    path('getMessages/<str:room>/', views.getMessages, name='getMessages'),
+]
